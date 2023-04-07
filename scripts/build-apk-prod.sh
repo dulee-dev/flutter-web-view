edd1b3bb1973a5033ac7cf538ab8d562ec43833b
dulee <duckyounglee@oden.kr>
2023-04-07 16:03:15 +0900

    ..

--- scripts/build-apk-prod.sh
+++ scripts/build-apk-prod.sh
@@ -1,1 +1,1 @@
-flutter build apk --release --target-platform=android-arm64 --no-tree-shake-icons --dart-define=WEB_VIEW_BASE_URL=https://oden.kr
+flutter build apk --release --target-platform=android-arm64 --no-tree-shake-icons --dart-define=WEB_VIEW_BASE_URL=https://flutter.dev
